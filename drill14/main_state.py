8205df6de46c0dcef7dcfed9c4c8c2d96b9bdc9a
Gnu <Gnu>
2018-11-22 16:35:23 +0900

    drill 14 화면에 공 띄우기
    화면에 공을 띄우고 부딪히면 소리가 나게 하였다.

--- drill14/main_state.py
+++ drill14/main_state.py
@@ -9,11 +9,26 @@ import game_world
 from boy import Boy
 from background import FixedBackground as Background
 #from background import InfiniteBackground as Background
+from ball import Ball
 
 name = "MainState"
 
 boy = None
 background = None
+balls = []
+
+
+def collide(a, b):
+    # fill here
+    left_a, bottom_a, right_a, top_a = a.get_bb()
+    left_b, bottom_b, right_b, top_b = b.get_bb()
+
+    if left_a > right_b: return False
+    if right_a < left_b: return False
+    if top_a < bottom_b: return False
+    if bottom_a > top_b: return False
+
+    return True
 
 
 def enter():
@@ -27,6 +42,11 @@ def enter():
 
     background.set_center_object(boy)
     boy.set_background(background)
+
+    global balls
+    balls = [Ball() for i in range(10)]
+    game_world.add_objects(balls, 1)
+
 
 
 
@@ -55,7 +75,11 @@ def handle_events():
 def update():
     for game_object in game_world.all_objects():
         game_object.update()
-
+    for ball in balls:
+        if collide(boy, ball):
+            balls.remove(ball)
+            boy.eat(ball)
+            game_world.remove_object(ball)
 
 def draw():
     clear_canvas()
